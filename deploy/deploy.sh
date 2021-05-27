15622f90083de59339c4c1f703dc5fcbb820f2bb
PrasanthMigobucks <servers@migobucks.com>
2021-05-27 11:32:43 +0530

    Deploy Script Error Fixed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,22 +18,7 @@ echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
 # comma seperated values of ip addresses.
 DEPLOY_SERVERS=$DEPLOY_SERVERS
 
-# lets split this string and convert this into array
-# In UNIX, we can use this commond to do this
-# ${string//substring/replacement}
-# our substring is "," and we replace it with nothing.
-#ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
 echo "ALL_SERVERS"
   ssh ubuntu@${DEPLOY_SERVERS} 'bash -s' < ./deploy/updateAndRestart.sh
-# Lets iterate over this array and ssh into each EC2 instance
-# Once inside.
-# 1. Stop the server
-# 2. Take a pull
-# 3. Start the server
-#for server in "${ALL_SERVERS[@]}"
-#do
-#  echo "deploying to ${server}"
-#  ssh ubuntu@${server} 'bash -s' < ./deploy/updateAndRestart.sh
-# done
 
 
--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -2,30 +2,14 @@
 
 # any future command that fails will exit the script
 set -e
-
-
-# stop the previous pm2
-# pm2 kill
-# npm remove pm2 -g
-# sudo apt-get install npm
+ 
 
 # Delete the old repo
 rm -rf /home/ubuntu/migobucksbrandservice
 
 # clone the repo again
 git clone https://gitlab.com/migobucks_personal/migobucksbrandservice.git
-
-#source the nvm file. In an non
-# If you are not using nvm, add the actual path like
-# PATH=/home/ubuntu/node/bin:$PATH
-# source /home/ubuntu/.nvm/nvm.sh
-
-#pm2 needs to be installed globally as we would be deleting the repo folder.
-# this needs to be done only once as a setup script. 
-# npm install pm2 -g
-# starting pm2 daemon
-# pm2 status
-
+ 
 cd /home/ubuntu/migobucksbrandservice
 
 #install npm packages
