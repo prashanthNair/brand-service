64bc72f0097f01b414e2a5e974ae77a9cb98ac1f
PrasanthMigobucks <servers@migobucks.com>
2021-05-28 17:33:15 +0530

    host url updated

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ class App {
         allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
         credentials: false,
         methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+        
         origin: '*',
         preflightContinue: false
     };
