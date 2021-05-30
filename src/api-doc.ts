6a62c751de2ec43a26989dde670002e4684d0517
jisnhu.ku <jikupnr@gmail.com>
2021-05-31 00:18:21 +0530

    base url

--- src/api-doc.ts
+++ src/api-doc.ts
@@ -11,7 +11,7 @@ export const swaggerDocument = {
   },
   servers:[
     {
-      "url" : "http://3.108.15.22:3000/api/v1",
+      "url" : "http://localhost:3000/api/v1",
       "description" : "Development server"
     }
   ]
