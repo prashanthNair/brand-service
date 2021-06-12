e91c3aefb0a4b5622323cc42375df338e292b9f9
Migobucks Servers <servers@migobucks.com>
2021-06-12 14:53:38 +0000

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,9 @@
 # any future command that fails will exit the script
 set -e
 # Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# eval $(ssh-agent -s)
+# echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+[ -z $SSH_AUTH_SOCK ] && `eval ssh-agent` && echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 
 # disable the host key checking.
